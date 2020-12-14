a260915130b75c7fb38f8c7a49e9cee5b6f3bc85
Andrew Tremblay <gloriousrocketship@gmail.com>
2020-12-13 22:16:58 -0500

    rewriting example to be more gif-y

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # wasm-ffmpeg-demo
 Demo of running ffmpeg on the web with WebAssembly
+
+Upload a file and convert it to a gif!
+
+Sample Videos taken from [sample-videos.com](https://www.sample-videos.com/)
--- src/App.js
+++ src/App.js
@@ -3,7 +3,18 @@ import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
 import './App.css';
 
 function App() {
-  const [videoSrc, setVideoSrc] = useState('');
+  const [fileURL, setFileURL] = useState(null);
+  const [fileObj, setFileObj] = useState(null);
+  const onChangeFile = (e) => {
+    let files = e.target.files;
+    if (files.length === 1) {
+      let file = files[0];
+      setFileURL(URL.createObjectURL(file));
+      setFileObj(file);
+    }
+  }
+
+  const [imgSrc, setImgSrc] = useState('');
   const [message, setMessage] = useState('Click Start to transcode');
   const ffmpeg = createFFmpeg({
     log: true,
@@ -11,19 +22,29 @@ function App() {
   const doTranscode = async () => {
     setMessage('Loading ffmpeg-core.js');
     await ffmpeg.load();
-    setMessage('Start transcoding');
-    ffmpeg.FS('writeFile', 'test.avi', await fetchFile('/flame.avi'));
-    await ffmpeg.run('-i', 'test.avi', 'test.mp4');
-    setMessage('Complete transcoding');
-    const data = ffmpeg.FS('readFile', 'test.mp4');
-    setVideoSrc(URL.createObjectURL(new Blob([data.buffer], { type: 'video/mp4' })));
+    setMessage('Starting transcoding');
+    ffmpeg.FS('writeFile', 'test.mp4', await fetchFile(fileObj));
+    await ffmpeg.run('-i', 'test.mp4', 'test.gif');
+    setMessage('Completing transcoding');
+    const data = ffmpeg.FS('readFile', 'test.gif');
+    setImgSrc(URL.createObjectURL(new Blob([data.buffer], { type: 'image/gif' })));
   };
+
   return (
     <div className="App">
+      <input id="myInput"
+        type="file"
+        accept="video/mp4" 
+        multiple={false}
+        onChange={onChangeFile}
+      />
       <p/>
-      <video src={videoSrc} controls></video><br/>
-      <button onClick={doTranscode}>Start</button>
+      <p>input video:</p>
+      {fileURL && <video src={fileURL} controls></video>}<br/><br/>
+      <button onClick={doTranscode}>Gif-ify</button>
       <p>{message}</p>
+      <p>output gif:</p>
+      <img src={imgSrc} /><br/>
     </div>
   );
 }
